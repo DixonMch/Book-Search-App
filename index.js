c4f67ff31c6ea97c5681c835798ffc91029b63f3
DixonMch <dixonmogaka@gmail.com>
2023-04-09 15:35:46 +0300

    This code defines a function called searchBooks(query) that searches for books using Google Books API based on a given search query. It constructs a URL using the API_URL constant and the search query. It then uses fetch() method to GET request to the API using that URL. The response from the API is a JSON object which is then converted to a JavaScript object using the response.json() method. The resulting data is used to populate a list of books in the HTML document.

--- index.js
+++ index.js
@@ -7,3 +7,37 @@ const bookPublishedDate = document.getElementById("book-published-date");
 const bookDescription = document.getElementById("book-description");
 const bookReviews = document.getElementById("book-reviews");
 const bookCover = document.getElementById("book-cover");
+
+const API_URL = "https://www.googleapis.com/books/v1/volumes";
+
+// Search for books by keyword
+function searchBooks(query) {
+  const url = `${API_URL}?q=${query}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      bookList.innerHTML = "";
+      data.items.forEach((item) => {
+        const book = document.createElement("li");
+        const bookLink = document.createElement("a");
+        const bookImage = document.createElement("img");
+
+        bookLink.href = "#";
+        bookLink.addEventListener("click", () => {
+          displayBookDetails(item);
+        });
+
+        if (item.volumeInfo.imageLinks) {
+          bookImage.src = item.volumeInfo.imageLinks.thumbnail;
+        } else {
+          bookImage.src = "https://via.placeholder.com/128x192.png?text=No+Cover";
+        }
+
+        bookLink.appendChild(bookImage);
+        book.appendChild(bookLink);
+        bookList.appendChild(book);
+      });
+    })
+    .catch((error) => console.log(error));
+}
